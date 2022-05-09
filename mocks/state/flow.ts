1565df57d02aa92a2abce4a00dd45f5e95da813f
Paul Allen <paul.allen@dashlane.com>
2022-05-09 16:45:26 -0400

    test: adding flowstore tests (#15)

---
+++ mocks/state/flow.ts
@@ -0,0 +1,12 @@
+export default {
+    screens: [{ name: 'test', type: 'INPUT' }],
+  screenHistory: [],
+  currenScreenIndex: 0,
+  previousScreenIndex: 0,
+  data: {},
+  settings: {
+    verbose: false,
+    version: '0.0.0',
+    strictValidation: true,
+  },
+};
